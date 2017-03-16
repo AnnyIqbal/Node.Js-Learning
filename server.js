b540a6a48db0d7a8d7e6331bd17523a18a68d7cd
Anny <quratulann_iqbal@hotmail.com>
2017-03-16 17:32:07 +0500

    tutorial 30 tk, agay k in new folder todo app

--- server.js
+++ server.js
@@ -66,12 +66,43 @@
 
 // app.listen(3000);
 
-/*********tutorial # 29***********query strings****************** */
+// /*********tutorial # 29***********query strings****************** */
+
+
+// var express = require('express');
+
+// var app = express();
+
+// app.set('view engine', 'ejs'); // template k lie engine set kia
+// app.use('/assets', express.static('assets')); // middleware to serve static files express.static(directory name)
+
+// app.get('/', function(req, res) {
+//     res.render('home');
+// });
+// app.get('/home', function(req, res) {
+//     res.render('home');
+// });
+// app.get('/about', function(req, res) {
+//     res.render(`about`);
+// });
+// app.get('/contact', function(req, res) {
+//     res.render(`contact`, {qs: req.query});
+// });
+// app.get('/profile/:name', function(req, res) {
+//     var data = {age: 29, job: 'ninja', hobbies: ['eating', 'sleeping', 'coding']};
+//     res.render('profile', {person: req.params.name, data: data});
+// });
+
+// app.listen(3000);
+
+/*********tutorial # 30***********post request****************** */
 
 
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs'); // template k lie engine set kia
 app.use('/assets', express.static('assets')); // middleware to serve static files express.static(directory name)
@@ -87,6 +118,10 @@ app.get('/about', function(req, res) {
 });
 app.get('/contact', function(req, res) {
     res.render(`contact`, {qs: req.query});
+});
+app.post('/contact', urlencodedParser, function(req, res) {
+    console.log(req.body);
+    res.render(`contact-success`, {data: req.body});
 });
 app.get('/profile/:name', function(req, res) {
     var data = {age: 29, job: 'ninja', hobbies: ['eating', 'sleeping', 'coding']};
